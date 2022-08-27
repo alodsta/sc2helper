c9b5477eee1ee974150102be0df4cd8aea1172e4
alodsta <alodsta@yandex.ru>
2022-08-27 16:00:39 +0300

    try import numba

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
+import numba
 
 
 with st.sidebar:
     '**Some sidebar**'
 
+
 with st.container():
     tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
-    st.button('Add new')
 
