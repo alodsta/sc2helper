f8cc0b9149f8eb244a2965e06769b8dc7aeea822
alodsta <51061861+alodsta@users.noreply.github.com>
2022-08-27 13:24:39 +0300

    Create main_cycle.py

---
+++ main_cycle.py
@@ -0,0 +1,13 @@
+class CurrentCycle:
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        pass
+    
+class VM:
+    def __init__(self, player_race):
+        pass
+    
+    def __anext__(self):
+        pass
