7467d73d4353f122dc419ed88100e1071687bca7
alodsta <alodsta@yandex.ru>
2022-08-27 19:12:44 +0300

    add pydantic in requirements

---
+++ index.py
@@ -0,0 +1,13 @@
+import asyncio
+import streamlit as st
+import numba
+
+
+if 'info' not in st.session_state:
+    st.session_state['info'] = {
+        'Zerg': {},
+        'Terr': {},
+        'Toss': {}
+    }
+else:
+    st.session_state['info'] = st.session_state.info
