9925bc1b3821609d3bc4ef5f02c5b210578d0716
alodsta <alodsta@yandex.ru>
2022-08-27 18:31:58 +0300

    exps with framework

--- app.py
+++ app.py
@@ -11,17 +11,8 @@ with st.container():
     tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
 
-def action():
-    st.session_state['clicker'] += 1
+# def action():
+#     st.session_state['clicker'] += 1
 
 
-if 'clicker' not in st.session_state:
-    st.session_state['clicker'] = 0
-
-if 'name' in st.session_state and bool(st.session_state.name):
-    st.write(f'hi, {st.session_state.name}')
-
-st.session_state['name'] = st.text_input('What is ur name?')
-
-st.button('reload', on_click=action)
-f'cliker: {st.session_state.clicker}'
+st.number_input('Choose number', key='my_number')
