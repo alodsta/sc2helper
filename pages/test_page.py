dee3232bc4a3761eb9f2e0888b0eb523a374122e
alodsta <alodsta@yandex.ru>
2022-08-27 18:36:37 +0300

    exps with framework

--- pages/test_page.py
+++ pages/test_page.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 if 'my_number' in st.session_state and st.session_state.my_number:
     st.write(f'Number is {st.session_state.my_number}')
+    st.session_state['my_number'] = st.session_state.my_number
 else:
     st.write('Number does not set, yet')
