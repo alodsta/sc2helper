8fad084b023f3a560eb4a0408ba6edcdb02d0c6a
alodsta <alodsta@yandex.ru>
2022-08-27 18:02:20 +0300

    exps with framework

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import asyncio
 import streamlit as st
 import numba
 
@@ -9,3 +10,14 @@ with st.sidebar:
 with st.container():
     tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
+
+def action():
+    pass
+
+
+st.session_state.name = st.text_input()
+st.session_state.cliker = 0
+
+f'hi, {st.session_state.name}'
+st.button('reload', on_click=lambda: (st.session_state.cliker := st.session_state.cliker + 1))
+f'cliker: {st.session_state.cliker}'
--- main_cycle.py
+++ main_cycle.py
@@ -4,10 +4,17 @@ class CurrentCycle:
     
     def __next__(self):
         pass
-    
+
+
 class VM:
     def __init__(self, player_race):
         pass
     
     def __anext__(self):
+        pass
+
+    def put(self, unit):
+        pass
+
+    def pop(self):
         pass
